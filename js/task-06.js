459afbdaca6edabf1a0c2766307706c883711636
axer4 <79149727+axer4@users.noreply.github.com>
2021-07-22 21:58:57 +0300

    Update task-06.js
    3, исправил ошибки

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,14 @@ const refs = {
 }
 const validation = refs.inputEl.addEventListener('change', (event) => {
     if (Number(refs.inputEl.dataset.length) !== refs.inputEl.value.length) {
-        refs.inputEl.classList.add('invalid');
+        refs.inputEl.classList.toggle('invalid');
     }
     else {
-        refs.inputEl.classList.replace('invalid','valid');
+        refs.inputEl.classList.toggle('invalid')
+        refs.inputEl.classList.replace('invalid', 'valid');
+        refs.inputEl.classList.add('valid');
+        
         
     }
+    
 });
-console.log(refs.inputEl.textContent.length);
